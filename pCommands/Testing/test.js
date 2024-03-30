7dc833aad489b24822d3148a753f01d45de9fc2e
Biktot <164114980+Biktot@users.noreply.github.com>
2024-03-30 09:29:13 +0800

    Add files via upload

---
+++ pCommands/Testing/test.js
@@ -0,0 +1,17 @@
+const { EmbedBuilder } = require("discord.js");
+module.exports = {
+  name: "test",
+  aliases: ["t"],
+  args: true,
+  owner: true,
+
+  async execute(message, client, args) {
+    const msg = args.join(" ");
+
+    const embed = new EmbedBuilder()
+      .setDescription(msg)
+      .setColor("#00FFFB");
+
+    message.reply({ embeds: [embed] });
+  },
+};
